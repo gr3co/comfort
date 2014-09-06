dd50d27f49bf9c54be58c55f3bdef971105fe58e
loopyz <lguo@andrew.cmu.edu>
2014-09-06 00:24:14 -0400

    reset

---
+++ Rooms/Rooms/RMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RMAppDelegate.h
+//  Rooms
+//
+//  Created by Lucy Guo on 9/6/14.
+//  Copyright (c) 2014 Lucy Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
