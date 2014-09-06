80131ec28ea8d77663eafa7d8cdd266d48949808
loopyz <lguo@andrew.cmu.edu>
2014-09-06 14:58:04 -0400

    fixed sizing of map view

--- ComfortMe/Cells/CMMapInfoTableViewCell.m
+++ ComfortMe/Cells/CMMapInfoTableViewCell.m
@@ -14,8 +14,11 @@
 {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
+        NSInteger offset = 0;
+        if (!iPhone5) offset = 9;
+        
         PFImageView *imageView = [[PFImageView alloc] initWithFrame:
-                                  CGRectMake(20, self.contentView.frame.size.height / 2 - 5, 60, 60) ];
+                                  CGRectMake(20, self.contentView.frame.size.height / 2 - 5 - offset, 60, 60) ];
         imageView.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin;
         imageView.layer.masksToBounds = YES;
         imageView.layer.rasterizationScale = [UIScreen mainScreen].scale;
@@ -36,7 +39,7 @@
 
         
         UILabel *label = [[UILabel alloc] initWithFrame:
-                          CGRectMake(48, self.contentView.frame.size.height / 2 - 10, self.frame.size.width - 50, 50)];
+                          CGRectMake(48, self.contentView.frame.size.height / 2 - 10 - offset, self.frame.size.width - 50, 50)];
         label.font = [UIFont fontWithName:@"Avenir" size:21];
         label.backgroundColor = [UIColor clearColor];
         label.textColor = [UIColor colorWithRed:62/255.0f green:68/255.0f blue:75/255.0f alpha:1.0f];
@@ -45,7 +48,7 @@
         label.textAlignment = NSTextAlignmentLeft;
         
         UILabel *etaLabel = [[UILabel alloc] initWithFrame:
-                          CGRectMake(25, self.contentView.frame.size.height / 2 + 20, self.frame.size.width - 50, 50)];
+                          CGRectMake(25, self.contentView.frame.size.height / 2 + 20 - offset, self.frame.size.width - 50, 50)];
         etaLabel.font = [UIFont fontWithName:@"Avenir-LightOblique" size:14];
         etaLabel.backgroundColor = [UIColor clearColor];
         etaLabel.textColor = [UIColor colorWithRed:62/255.0f green:68/255.0f blue:75/255.0f alpha:1.0f];
