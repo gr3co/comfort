230531aac5d625c422440b2f501c818c4c25c369
Stephen Greco <sgreco94@gmail.com>
2014-09-06 13:09:21 -0400

    Callbutton

--- ComfortMe/Cells/CMCallButtonTableViewCell.m
+++ ComfortMe/Cells/CMCallButtonTableViewCell.m
@@ -15,6 +15,15 @@
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
         // Initialization code
+        _callButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        UIImage *btnImage = [UIImage imageNamed:@"ComfortButton"];
+        [_callButton setImage:btnImage forState:UIControlStateNormal];
+        _callButton.contentMode = UIViewContentModeScaleToFill;
+        _callButton.frame = CGRectMake(0, 0, self.frame.size.width, self.frame.size.height + 10);
+        
+        [_callButton addTarget:_delegate action:@selector(callButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+        
+        [self.contentView addSubview:_callButton];
     }
     return self;
 }
@@ -27,7 +36,7 @@
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated
 {
     [super setSelected:selected animated:animated];
-
+    
     // Configure the view for the selected state
 }
 
